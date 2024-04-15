0a561089ff0555020ae3229d19ccb6b79bad80cb
BourhailHamza <bourhailhamza@gmail.com>
2024-04-15 11:41:23 +0200

    [Add] Project structure

---
+++ src/main/java/cyber/nexus/CyberNexus/CyberNexusApplication.java
@@ -0,0 +1,13 @@
+package cyber.nexus.CyberNexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberNexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cyber/nexus/CyberNexus/CyberNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package cyber.nexus.CyberNexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
