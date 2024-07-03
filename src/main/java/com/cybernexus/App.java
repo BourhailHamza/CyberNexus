918f46263d691fba003b0e534f928190d91bc458
Bourhail Hamza <bourhailhamza@gmail.com>
2024-07-03 15:48:50 +0200

    [+] Dependencies hibernate, sqlite and spring

---
+++ src/main/java/com/cybernexus/App.java
@@ -0,0 +1,13 @@
+package com.cybernexus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
